e1d2aef8c84bbe3aed26c9918c5d580c8faf1dd7
Iafcy <frankiecheung512@gmail.com>
2023-03-19 13:44:19 +0800

    Added Message component.

---
+++ src/lib/components/Message.svelte
@@ -0,0 +1,17 @@
+<script>
+    export let message;
+    export let role;
+</script>
+
+<section class="p-4 items-center justify-start flex
+    {role == 'AI' ? 'dark:bg-stone-800' : 'dark:bg-stone-900'}"
+>
+    <div class="flex items-center justify-center rounded-full mr-6 w-9 h-9 self-start
+        {role == 'AI' ? 'bg-green-700' : 'bg-blue-600'}"
+    >
+        {role == "AI" ? "A" : "U"}
+    </div>
+    <div class="message flex-1">
+        <p>{message}</p>
+    </div>
+</section>
--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -1,2 +1,14 @@
-<h1>Welcome to SvelteKit</h1>
-<p>Visit <a href="https://kit.svelte.dev">kit.svelte.dev</a> to read the documentation</p>
+<script>
+    import Message from "$lib/components/Message.svelte";
+</script>
+
+<main>
+    <Message message={"Hi"} role={"AI"} />
+    <Message message={"Hi"} role={"User"} />
+    <Message message={"Hi"} role={"AI"} />
+    <Message
+        message={"Lorem ipsum dolor sit amet consectetur adipisicing elit. Voluptate, dolore? Alias consequuntur inventore nostrum maiores earum itaque ab eveniet rerum.Lorem ipsum dolor sit amet consectetur adipisicing elit. Voluptate, dolore? Alias consequuntur inventore nostrum maiores earum itaque ab eveniet rerum.Lorem ipsum dolor sit amet consectetur adipisicing elit. Voluptate, dolore? Alias consequuntur inventore nostrum maiores earum itaque ab eveniet rerum.Lorem ipsum dolor sit amet consectetur adipisicing elit. Voluptate, dolore? Alias consequuntur inventore nostrum maiores earum itaque ab eveniet rerum.Lorem ipsum dolor sit amet consectetur adipisicing elit. Voluptate, dolore? Alias consequuntur inventore nostrum maiores earum itaque ab eveniet rerum.Lorem ipsum dolor sit amet consectetur adipisicing elit. Voluptate, dolore? Alias consequuntur inventore nostrum maiores earum itaque ab eveniet rerum."}
+        role={"User"}
+    />
+    <Message message={"Hi"} role={"AI"} />
+</main>
